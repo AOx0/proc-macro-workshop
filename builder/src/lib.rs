8b2aa029ac0d1c6db4a01acb98674bc4c98a4122
AOx0 <50227494+AOx0@users.noreply.github.com>
2022-06-12 16:39:27 -0500

    Cleanup

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields, GenericArgument};
+use syn::{parse_macro_input, Data, DeriveInput, GenericArgument};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -15,77 +15,69 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let mut setters = vec![];
     let mut build_items = vec![];
 
-    if let Data::Struct(members) = input.data {
-        let fields = members.fields;
+    if let Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { named, .. }),
+        ..
+    }) = input.data
+    {
+        for field in named {
+            let syn::Field { ty, ident, .. } = field;
 
-        if let Fields::Named(fields) = fields {
-            for field in fields.named {
-                let syn::Field { ty, ident, .. } = field;
+            let mut optional_value = None;
+            let is_option = if let syn::Type::Path(ty) = ty.clone() {
+                let segments = ty.path.segments.first().unwrap();
+                let option_ident = segments.ident == "Option";
 
-                let mut optional_value = None;
-                let is_option = if let syn::Type::Path(ty) = ty.clone() {
-                    let segments = ty.path.segments;
-                    let segments = segments.first().unwrap();
-                    let option_ident = segments.ident == "Option";
-
-                    let generic_arg =
-                        if let syn::PathArguments::AngleBracketed(v) = segments.arguments.clone() {
-                            if let Some(GenericArgument::Type(ty)) = v.args.first() {
-                                optional_value = Some(ty.to_owned());
-                                true
-                            } else {
-                                false
-                            }
+                let generic_arg =
+                    if let syn::PathArguments::AngleBracketed(v) = segments.arguments.clone() {
+                        if let Some(GenericArgument::Type(ty)) = v.args.first() {
+                            optional_value = Some(ty.to_owned());
+                            true
                         } else {
                             false
-                        };
-
-                    option_ident && generic_arg
-                } else {
-                    false
-                };
-
-                let builder_field = if !is_option {
-                    quote!(pub #ident : Option< #ty >)
-                } else {
-                    quote!(pub #ident : #ty)
-                };
-
-                let builder_field_init = quote!(#ident: None);
-                let setter = if is_option {
-                    quote! {
-                        fn #ident (&mut self, #ident: #optional_value ) -> &mut Self {
-                            self. #ident = Some(#ident);
-                            self
                         }
-                    }
-                } else {
-                    quote! {
-                        fn #ident (&mut self, #ident: #ty ) -> &mut Self {
-                            self. #ident = Some(#ident);
-                            self
-                        }
-                    }
-                };
+                    } else {
+                        false
+                    };
 
-                let err_msg = format!("Error: {} wasn't initialized", quote! {#ident});
+                option_ident && generic_arg
+            } else {
+                false
+            };
 
-                let build_item = if !is_option {
-                    quote!(
-                        #ident: self.#ident.to_owned().ok_or(#err_msg)?
-                    )
-                } else {
-                    quote!(
-                        #ident: self.#ident.to_owned()
-                    )
-                };
-                build_items.push(build_item);
+            let builder_field = if !is_option {
+                quote!(pub #ident : Option< #ty >)
+            } else {
+                quote!(pub #ident : #ty)
+            };
 
-                builder_fields.push(builder_field);
-                builder_fields_inits.push(builder_field_init);
-                setters.push(setter);
-            }
+            let builder_field_init = quote!(#ident: None);
+            let arg_type = if is_option { optional_value } else { Some(ty) };
+
+            let setter = quote! {
+                fn #ident (&mut self, #ident: #arg_type ) -> &mut Self {
+                    self. #ident = Some(#ident);
+                    self
+                }
+            };
+
+            let build_item = if !is_option {
+                quote!(
+                    #ident: self.#ident.to_owned().ok_or(concat!("Error: ", stringify!(#ident), " was not initialized"))?
+                )
+            } else {
+                quote!(
+                    #ident: self.#ident.to_owned()
+                )
+            };
+            build_items.push(build_item);
+
+            builder_fields.push(builder_field);
+            builder_fields_inits.push(builder_field_init);
+            setters.push(setter);
         }
+    } else {
+        unimplemented!()
     }
 
     let nothing = quote!(
